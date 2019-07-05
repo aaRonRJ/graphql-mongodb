a6e006df633b9b4fbf80270ec3aec2f831fd0be1
Aarón RJ <aaron@quantumstudio.es>
2019-07-05 14:32:05 +0200

    - Create example with graphQL and mongoDB.

---
+++ src/database.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+export async function connect() {
+    try {
+        await mongoose.connect('mongodb://localhost/mongodbgraphql', {
+            useNewUrlParser: true
+        });
+
+        console.log('>>> DB is connected');
+    } catch(e) {
+        console.log('Something goes worng!');
+        console.log(e);
+    }
+}
+
+
---
+++ src/sample.js
@@ -0,0 +1,5 @@
+export const tasks = [
+    { _id: 0, title: 'Do homework', description: 'lorem ipsun', number: 100 },
+    { _id: 1, title: 'Run', description: 'lorem ipsun', number: 100 },
+    { _id: 2, title: 'Sleep', description: 'lorem ipsun', number: 100 },
+]
